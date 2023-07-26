5b29d69313ef43b2bffb33cc49b9a49cb2cfaffa
Khor Kean Teng <u2004763@siswa.um.edu.my>
2023-07-26 11:32:09 +0800

    Updates

---
+++ marker_cluster_plot.py
@@ -0,0 +1,60 @@
+import streamlit as st
+import leafmap.foliumap as leafmap
+import pandas as pd
+
+st.set_page_config(layout="wide")
+
+st.sidebar.info(
+    """
+    - GitHub repository: 
+    """
+)
+
+st.sidebar.title("Created By")
+st.sidebar.info(
+    """
+  Khor Kean Teng
+  - Intern, DGA, JPS
+    """
+)
+
+st.title("Flood Incidents in Malaysia")
+
+button = st.slider("Year", 2015,2022,2022)
+data = pd.read_csv('Flood Data Updated Geocoded.csv')
+
+if button == 2015:
+    data = data[data['Year'] == 2015]
+elif button == 2016:
+    data = data[data['Year'] == 2016]
+elif button == 2017:
+    data = data[data['Year'] == 2017]
+elif button == 2018:
+    data = data[data['Year'] == 2018]
+elif button == 2019:
+    data = data[data['Year'] == 2019]
+elif button == 2020:
+    data = data[data['Year'] == 2020]
+elif button == 2021:
+    data = data[data['Year'] == 2021]
+else:
+    data = data[data['Year'] == 2022]
+
+with st.expander("Source Code (Click to Expand)"):
+    with st.echo():
+
+        m = leafmap.Map(center=[4, 108], zoom=5)
+        
+        cities = data
+        regions = 'countries.geojson'
+
+        m.add_geojson(regions)
+        m.add_points_from_xy(
+            cities,
+            x="Longitude",
+            y="Latitude",
+            icon_names=['gear', 'map', 'leaf', 'globe'],
+        )
+
+
+m.to_streamlit(height=700)
